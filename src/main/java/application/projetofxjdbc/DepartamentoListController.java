a9a3909fde24f7dbc8ce1ea1b5b2b625659dd294
Adriano Crosoletto <adrianocrosoletto@outlook.com>
2022-07-14 23:37:52 -0300

    Update departamento

--- src/main/java/application/projetofxjdbc/DepartamentoListController.java
+++ src/main/java/application/projetofxjdbc/DepartamentoListController.java
@@ -5,6 +5,7 @@ import application.projetofxjdbc.Util.Utils;
 import application.projetofxjdbc.listeners.DataChangeListener;
 import application.projetofxjdbc.model.entities.Departamento;
 import application.projetofxjdbc.model.services.DepartmentService;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,10 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -31,7 +29,6 @@ public class DepartamentoListController implements Initializable, DataChangeList
     private DepartmentService service;
 
 
-
     //Referencia para a tela do Lista de Depto
     @FXML
     private TableView<Departamento> tableViewDepartamento;
@@ -41,6 +38,9 @@ public class DepartamentoListController implements Initializable, DataChangeList
 
     @FXML
     private TableColumn<Departamento, String> tableColumnNome;
+
+    @FXML
+    private TableColumn<Departamento, Departamento> tableColumnEdit;
 
     @FXML
     private Button btNovo;
@@ -82,6 +82,7 @@ public class DepartamentoListController implements Initializable, DataChangeList
         List<Departamento> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);//Pega os dados da lista
         tableViewDepartamento.setItems(obsList);
+        initEditButtons();//acrescenta o botão em cada linha da tabela.
     }
 
     //Metodo para o formulario
@@ -116,4 +117,24 @@ public class DepartamentoListController implements Initializable, DataChangeList
     public void onDataChanged() {
         updateTableView();
     }
+
+    private void initEditButtons() {
+        tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEdit.setCellFactory(param -> new TableCell<Departamento, Departamento>() {
+            private final Button button = new Button("edit");
+            @Override
+            protected void updateItem(Departamento obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createdDialogForm(
+                                obj, "FormularioDepartamento.fxml",Utils.currentStage(event)));
+            }
+        });
+    }
+
 }
