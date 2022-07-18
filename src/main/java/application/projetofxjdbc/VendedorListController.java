276db59e73bf99debd7e878732888ea1beb8bc07
Adriano Crosoletto <adrianocrosoletto@outlook.com>
2022-07-18 19:34:58 -0300

    Tela de vendedor

--- src/main/java/application/projetofxjdbc/VendedorListController.java
+++ src/main/java/application/projetofxjdbc/VendedorListController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -36,6 +37,15 @@ public class VendedorListController implements Initializable, DataChangeListener
 
     @FXML
     private TableColumn<Vendedor, String> tableColumnNome;
+
+    @FXML
+    private TableColumn<Vendedor, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Vendedor, Date> tableColumnDataNasc;
+
+    @FXML
+    private TableColumn<Vendedor, Double> tableColumnSalarioBase;
 
     @FXML
     private TableColumn<Vendedor, Vendedor> tableColumnEdit;
@@ -72,7 +82,11 @@ public class VendedorListController implements Initializable, DataChangeListener
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnDataNasc.setCellValueFactory(new PropertyValueFactory<>("dataNasc"));
+        Utils.formatTableColumnDate(tableColumnDataNasc, "dd/MM/yyyy");
+        tableColumnSalarioBase.setCellValueFactory(new PropertyValueFactory<>("salarioBase"));
+        Utils.formatTableColumnDouble(tableColumnSalarioBase, 2);
     }
 
     //Acessa o service carrega os dpto e jogo os dto na obsList
